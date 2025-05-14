920dee54e45c0831d8f67c58636085439aa57332
Vitor Carlos da Silva <vitorcarlossilva62@gmail.com>
2025-05-14 22:35:28 +0000

    Nível aventureiro

--- xadrez1.c
+++ xadrez1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main ()
-//Movendo o bispo na diagonal superior direita 5 vezes
+int main()
+// Movendo o bispo na diagonal superior direita 5 vezes
 {
     int j = 0;
     while (j < 5)
@@ -11,7 +11,7 @@ int main ()
     }
     printf("\n");
 
-    //Movendo a torre 5 casas para a direita
+    // Movendo a torre 5 casas para a direita
 
     for (int t = 0; t < 5; t++)
     {
@@ -20,7 +20,7 @@ int main ()
 
     printf("\n");
 
-    //Movendo a rainha 8 vezes para a esquerda 
+    // Movendo a rainha 8 vezes para a esquerda
 
     int i = 0;
     do
@@ -28,8 +28,23 @@ int main ()
         printf("Esquerda\n");
         i++;
     } while (i < 8);
-    
+
     printf("\n");
 
-    
+    //Movendo o cavalo duas vezes para cima e uma vez para a esquerda fromando um L
+
+    int c = 1;
+
+    while (c--)
+    {
+        for (int n = 0; n < 2; n++)
+        {
+            printf("Cima,"); //imprimindo cima duas vezes)
+        }
+        printf("Esquerda"); // imprimindo esquerda uma vez formando o movimento em L
+    }
+
+    printf("\n");
+
+
 }
